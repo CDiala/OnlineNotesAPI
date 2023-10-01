bc9bd34b5a2ce610170bd100f5b431d5f3023df7
CDiala <dialachibuzo@yahoo.com>
2023-10-02 00:55:27 +0100

    feat: Create users app, setup user model

---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class User(models.Model):
+    '''
+    Custom user model that supports email authentication.
+    '''
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    is_email_valid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
