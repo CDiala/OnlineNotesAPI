6671ff5d943c2e3cfbe148aeeaf9feb380ffd00b
CDiala <dialachibuzo@yahoo.com>
2023-10-04 22:03:06 +0100

    feat: Add new notes endpoint

--- notes/urls.py
+++ notes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("notes/", views.get_notes)
+    path("notes/", views.get_notes),
+    path("notes/<int:note_id>/", views.get_note_by_id),
 ]
