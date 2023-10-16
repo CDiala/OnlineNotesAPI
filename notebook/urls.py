e8718ff3425067dfba7a2c3a36dba86a83606859
CDiala <dialachibuzo@yahoo.com>
2023-10-16 18:51:46 +0100

    refactor: Remove comment, add name to routes

--- notebook/urls.py
+++ notebook/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from . import views
 
+''' Setup endpoint routes '''
 urlpatterns = [
-    path("notes/", views.NoteList.as_view()),
-    path("notes/<int:note_id>/", views.NoteDetail.as_view()),
+    path("notes/", views.NoteList.as_view(), name="note-list"),
+    path("notes/<int:note_id>/", views.NoteDetail.as_view(), name="note-detail"),
     path('pdf_view/', views.ViewPDF.as_view(), name="pdf_view"),
     path('pdf_download/', views.DownloadPDF.as_view(), name="pdf_download"),
     path('csv_download/', views.DownloadCSV.as_view(), name="csv_download"),
     path('send_attachment/', views.SendAttachment.as_view(), name="send_attachment"),
     path('', views.show_uploader, name="show_uploader"),
-    # if this crashes when data is available, change owner_id to pk and reflect same change in view
 ]
