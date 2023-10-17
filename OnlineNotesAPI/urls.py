231a9a54d058e99a1126b84546a09c2f8eec5ba3
CDiala <dialachibuzo@yahoo.com>
2023-10-17 23:38:34 +0100

    feat: Modify 404 page & static image settings

--- OnlineNotesAPI/urls.py
+++ OnlineNotesAPI/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf.urls.static import static
 
 from notebook import views
 
@@ -29,4 +31,4 @@ urlpatterns = [
     path('api/v1/', include('core.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
     re_path(r'^.*$', views.redirect_noexist, name='wildcard_route'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
